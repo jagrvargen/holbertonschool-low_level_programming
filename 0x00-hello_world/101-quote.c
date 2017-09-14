830f12ab80a5ca2c54b46982c2f5f2f6205f9629
jesse.hedden <229@holbertonschool.com>
2017-09-14 02:36:17 +0000

    fixed quote hopefully

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 
 /**
-* main - entry point
+* main - Entry point
+*
+* Return: always 0 (success)
 */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return 1;
+	return (1);
 }
