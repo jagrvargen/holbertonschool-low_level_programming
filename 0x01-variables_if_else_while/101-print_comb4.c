fb31d0b5f0844346b8f41d17f6cb0f00b32ad566
jesse.hedden <229@holbertonschool.com>
2017-09-15 00:23:26 +0000

    added print_comb4.c file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,27 @@ int main(void)
 	int i;
 	int j;
 	int k;
-	int l;
 
-	for (i = )
+	for (i = 48; i <= 55; i++)
+	{
+		j = 49;
+		for (; j <= 56; j++)
+		{
+			k = 50;
+			for (; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+					break;
+				else {
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
