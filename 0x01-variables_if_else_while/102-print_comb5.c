ccee501c0d7e23af8973943513c09de053d4ae19
jesse.hedden <229@holbertonschool.com>
2017-09-15 16:10:03 +0000

    final fix of print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,30 +3,43 @@
 /**
  * main - Entry point
  *
- * void - does not accept args
+ * void - takes no arguments
  *
  * Return: always 0 (success)
- *
  */
 int main(void)
 {
 	int i;
 	int j;
+	int k;
+	int l;
+	int m;
+	int n;
 
-			for (i = '0'; i <= '9'; i++)
-			{
-				for (j = '0'; j <= '9'; j++)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(k);
-					putchar(l);
-
-					putchar(',');
-					putchar(' ');
-				}
-			}
+	i = 0;
+	j = 0;
+	while (i <= 99)
+	{
+		while (j < 99)
+		{
+			j++;
+			k = i / 10;
+			l = i % 10;
+			m = j / 10;
+			n = j % 10;
+			putchar(k +'0');
+			putchar(l + '0');
+			putchar(' ');
+			putchar(m + '0');
+			putchar(n + '0');
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+		j = i + 1;
+		i++;
+	}
 	putchar('\n');
 	return (0);
 }
