eccbf54f1edbb1301b690d273839ac37f3d491fa
jesse.hedden <229@holbertonschool.com>
2017-09-19 17:15:02 +0000

    added file add.c with readme update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: the result of a + b
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -17,3 +17,5 @@
 8. jack_bauer - Prints every minute of the day of Jack Bauer, from 00:00 to 23:59.
 
 9. times_table - prints the 9 times table, starting with 0.
+
+10. add - Adds two integers and returns the result.
