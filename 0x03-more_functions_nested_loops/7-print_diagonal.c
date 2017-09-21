e399252863a207e655708423dfa270af2711cde7
jesse.hedden <229@holbertonschool.com>
2017-09-21 19:59:23 +0000

    fixed upper limit in print_diagonal.c

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
@@ -22,6 +22,6 @@ void print_diagonal(int n)
 			_putchar('\\');
 		_putchar('\n');
 	}
-	if (n < 0)
+	if (n <= 0)
 		_putchar('\n');
 }
