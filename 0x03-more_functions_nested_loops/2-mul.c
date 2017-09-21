6ca66adcb5ff496307bfe66846878c0a6c72001f
jesse.hedden <229@holbertonschool.com>
2017-09-21 15:35:13 +0000

    added file mul.c with readme update

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: Product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x03-more_functions_nested_loops/README.md
+++ 0x03-more_functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 0. _isupper.c - Checks whether or not a letter is uppercase. Returns 1 for yes, 0 for no.
 
 1. _isdigit.c - Check if the argument is a digit.
+
+2. mul.c - Multiplies two integers.
