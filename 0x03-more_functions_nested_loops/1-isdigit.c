2faba332c5a45dd56ad472e19c31886a10fa933b
jesse.hedden <229@holbertonschool.com>
2017-09-21 15:28:07 +0000

    added file isdigit.c with readme update

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
-
 /**
- * _isupper - check if argument is uppercase letter.
+ * _isdigit - checks if argument is ascii char 0 - 9
  *
  * @c: single integer argument
  *
  * Return: 1 if true, 0 if false.
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	return (c >= '0' && c <= '9');
 }
--- 0x03-more_functions_nested_loops/README.md
+++ 0x03-more_functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 0. _isupper.c - Checks whether or not a letter is uppercase. Returns 1 for yes, 0 for no.
+
+1. _isdigit.c - Check if the argument is a digit.
