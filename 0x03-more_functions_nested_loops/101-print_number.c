c98238f1d9935e6bbcedb1b01205b1f16c78364b
jesse.hedden <229@holbertonschool.com>
2017-09-22 22:22:01 +0000

    Altered print_number.c code to be non-recursive

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,36 @@
 #include "holberton.h"
+#include <stdio.h>
 
-/**
- * print_number - prints an integer using _putchar
- *
- * @n: takes an integer as an argument
- *
- * Return: void
- */
 void print_number(int n)
 {
+	int x;
+	int counter;
+	int divisor;
+
 	if (n < 0)
 	{
 		n *= -1;
 		_putchar('-');
-		print_number(n / 10);
 	}
-	else if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	else if (n == 0)
+		_putchar('0');
+
+	x = n;
+	counter = 0;
+	while (x > 0)
+	{
+		x /= 10;
+		++counter;
+	}
+	divisor = 1;
+	while (counter > 1)
+	{
+		divisor *= 10;
+		counter--;
+	}
+	while (divisor > 0)
+	{
+		_putchar(n / divisor % 10 + '0');
+		divisor /= 10;
+	}
 }
