8c96f50b11e60d403c9aae2cc7f1630bd9c6d675
jesse.hedden <229@holbertonschool.com>
2017-09-25 23:16:01 +0000

    added comments to reset_to_98

--- 0x04-pointers-arrays-strings/0-reset_to_98.c
+++ 0x04-pointers-arrays-strings/0-reset_to_98.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+
+/**
+ * reset_to_98 - updates the value a pointer points to to 98.
+ *
+ * @*n: takes pointer to int as parameter
+ *
+ * Return - void
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
