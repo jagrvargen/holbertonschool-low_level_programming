fdabf4a890e25207143fde0ea31f3d5ec7076a6b
jesse.hedden <229@holbertonschool.com>
2017-09-26 17:58:36 +0000

    fixed incrementing issue with puts2

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,8 @@ void puts2(char *str)
 {
 	int j;
 
-	for (j = 0; *(str + j) != '\0'; j += 2)
-		_putchar(*(str + j));
+	for (j = 0; *(str + j) != '\0'; j++)
+		if (j % 2 == 0)
+			_putchar(*(str + j));
 	_putchar('\n');
 }
