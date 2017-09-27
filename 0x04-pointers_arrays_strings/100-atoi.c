0713f224fb2ff147e1081ec5e0c2fe369f393e83
jesse.hedden <229@holbertonschool.com>
2017-09-27 01:36:07 +0000

    atoi fix

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ int _atoi(char *s)
 	}
 	if (*s == '\0')
 		return (total);
-	while (*s >= '0' && *s <= '9')
+	while (*s >= '0' && *s <= '9' && *s != '\0')
 	{
 		total = total * 10 + (*s - '0');
 		s++;
