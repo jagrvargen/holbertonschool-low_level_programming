dbdb99ff5356f7ff935135660ce4f4d32d999f50
jesse.hedden <229@holbertonschool.com>
2017-10-02 20:16:44 +0000

    added file strchr.c with readme update

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates a character in a string
+ *
+ * @s: Pointer to a string.
+ * @c: Char to be located
+ *
+ * Return: A pointer to a char.
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 0. memset - Fills memory with a constant byte.
 
 1. memcpy - Copies n bytes from memory area src to memory area dest.
+
+2. strchr - Locates a character in a string and returns a point to the char c in the string s, or NULL if not found.
