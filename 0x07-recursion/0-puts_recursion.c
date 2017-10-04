511662ecade262e66710492d1e743702effa4360
jesse.hedden <229@holbertonschool.com>
2017-10-04 22:41:16 +0000

    fixed return statment

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -10,8 +10,11 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		_putchar('\n');
+		return;
+	}
 	_putchar(*s);
-	if (*s != '\0')
-		_puts_recursion(s + 1);
+	s++;
+	_puts_recursion(s);
 }
