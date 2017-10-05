4a0b38353dfcd1a4e9d22b1ccff6e9283c908c4a
jesse.hedden <229@holbertonschool.com>
2017-10-05 21:21:51 +0000

    added comments to args.c

--- 0x09-arc_argv/1-args.c
+++ 0x09-arc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the numbver of arguments passed to it
+ *
+ * @argc: An integer value
+ * @argv: A pointer to an array of strings
+ *
+ * Return: Always 0 (success)
+ */
 int main(int argc, char *argv[] __attribute__ ((unused)))
 {
 	printf("%d\n", argc - 1);
