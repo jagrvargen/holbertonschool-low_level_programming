a72aa9cfc579ff716e43cf32f593af11eb412f5b
jesse.hedden <229@holbertonschool.com>
2017-10-10 01:22:16 +0000

    fixed counting issue in change.c

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -2,58 +2,39 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/**
- * main - Prints the minimum number of coins needed to make change for an
- * amount of money.
- *
- * @argc: An integer value
- * @argv: A pointer to an array of strings
- *
- * Return: An integer value.
- */
 int main(int argc, char *argv[])
 {
-	int count;
+	int total;
 	int num;
 
-	count = 0;
+	total = 0;
+	num = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num = atoi(argv[1]);
-	printf("num is %d\n", num);
-	if (num < 0)
-		printf("%d\n", 0);
-	while (num > 0)
+	while (num >= 25)
 	{
-		if (num / 25 >= 0)
-		{
-			count += num / 25;
-			num /= 25;
-		}
-		else if (num / 10 >= 0)
-		{
-			count += num / 10;
-			num /= 10;
-		}
-		else if (num / 5 >= 0)
-		{
-			count += num / 5;
-			num /= 5;
-		}
-		else if (num / 2 >= 0)
-		{
-			count += num / 2;
-			num /= 2;
-		}
-		else
-		{
-			count += num / 1;
-			num = 0;
-		}
+		num -= 25;
+		total++;
 	}
-	printf("%d\n", count);
+	while (num >= 10)
+	{
+		num -= 10;
+		total++;
+	}
+	while (num >= 5)
+	{
+		num -= 5;
+		total++;
+	}
+	while (num >= 2)
+	{
+		num -= 2;
+		total++;
+	}
+	total += num / 1;
+	printf("%d\n", total);
 	return (0);
 }
