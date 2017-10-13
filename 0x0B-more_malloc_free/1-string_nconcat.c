1c2c699dab771163b3270fc8388f5727b477c73c
jesse.hedden <229@holbertonschool.com>
2017-10-13 06:00:49 +0000

    fixed all

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,32 @@
 #include "holberton.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 int _strlen(char *s);
 
-/**
- * string_nconcat - Concatenates two strings. s2 can be up to n bytes in length
- *
- * @s1: A pointer to a string.
- * @s2: A pointer to a string.
- * @n: An unsigned integer.
- *
- * Return: A pointer to a string.
- */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *point;
-	int i;
-	unsigned int j;
-	unsigned int len;
+	char *pointer;
+	unsigned int i, j;
+	unsigned int len1, len2;
 
-	if (n <= (unsigned int) _strlen(s2))
-		len = _strlen(s1) + n + 1;
-	else if (n > (unsigned int) _strlen(s2))
-		len = _strlen(s1) + _strlen(s2) + 1;
-	point = malloc(sizeof (char) * len);
-	if (point == NULL)
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n <= 0)
+		return (s1);
+	else if (len2 < n)
+		n = (unsigned int) _strlen(s2);
+	pointer = malloc(sizeof(char) * (len1 + n + 1));
+	if (pointer == NULL)
 		return (NULL);
-	for (i = 0; (unsigned int) i < len; i++)
-		point[i] = '0';
-	for (i = 0; s1[i] != '\0'; i++)
-		point[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		pointer[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
-		point[i] = s2[j];
+		pointer[i] = s2[j];
 		i++;
 	}
-	point[i] = '\0';
-	return (point);
+	pointer[i] = '\0';
+	return (pointer);
 }
 
 /**
@@ -50,9 +40,9 @@ int _strlen(char *s)
 {
         int i;
 
-	if (s == NULL)
-		i = 0;
+        if (s == NULL)
+                i = 0;
         for (i = 0; s[i] != '\0';)
-		i++;
+                i++;
         return (i);
 }
