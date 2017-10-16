009db12bbb2742be09020ea738b2952839822a78
jesse.hedden <229@holbertonschool.com>
2017-10-16 21:23:36 +0000

    added header file object_like_macro with readme update

---
+++ 0x0C-preprocessor/README.md
@@ -0,0 +1,1 @@
+0. object_like_macro - A header file that defines a macro named SIZE as an abbreviation for the token 1024.
---
+++ 0x0C-preprocessor/holberton.h
@@ -0,0 +1,78 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _isdigit(int c);
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
+
+void set_string(char **s, char *to);
+
+void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmember, unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
