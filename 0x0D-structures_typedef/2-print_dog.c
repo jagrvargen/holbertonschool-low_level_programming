ca1e734916d3dd367cfbc9d663da94c4d5e14b0f
jesse.hedden <229@holbertonschool.com>
2017-10-17 04:53:09 +0000

    added labels to print statements

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,12 +14,12 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else
-		printf("%s\n", d->name);
-	printf("%f\n", d->age);
+		printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	else
-		printf("%s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }
