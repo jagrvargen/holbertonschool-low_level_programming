5d6cb06aadd1d58ddf457afe864464fdf23a61da
jesse.hedden <229@holbertonschool.com>
2017-10-17 04:36:41 +0000

    added file print_dog.c with readme update

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Prints a struct dog.
+ *
+ * @d: A pointer to a struct
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("%s\n", d->name);
+	if (d->age == 0)
+		printf("Age: (nil)");
+	else
+		printf("%f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("%s\n", d->owner);
+}
--- 0x0D-structures_typedef/README.md
+++ 0x0D-structures_typedef/README.md
@@ -1,3 +1,5 @@
 0. dog - A typdef of a struct called 'dog' with the elements char * name, float age, and char *owner.
 
 1. init_dog.c - Initializes a variable of type struct dog.
+
+2. print_dog.c - Function that prints a struct dog.
