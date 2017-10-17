392192889310c0915ef4b50796cf29f47765ef53
jesse.hedden <229@holbertonschool.com>
2017-10-17 22:03:04 +0000

    added file new_dog.c with header and readme update

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+int _strlen(char *s);
+char *_strdup(char *s);
 /**
  * new_dog - Creates a new dog
  *
@@ -13,13 +15,80 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *pntr;
+	char *npntr;
+	char *opntr;
 
+	if (name == NULL)
+		return (NULL);
 	pntr = malloc(sizeof(struct dog));
 	if (pntr == NULL)
 		return (NULL);
-	pntr->name = name;
+	npntr = malloc(sizeof(char) * _strlen(name));
+	if (npntr == NULL)
+	{
+		free(pntr);
+		return (NULL);
+	}
+	npntr = _strdup(name);
+	opntr = malloc(sizeof(char) * _strlen(owner));
+	if (opntr == NULL)
+	{
+		free(npntr);
+		free(pntr);
+		return (NULL);
+	}
+	opntr = _strdup(owner);
+	pntr->name = npntr;
 	pntr->age = age;
-	pntr->owner = owner;
-
+	pntr->owner = opntr;
 	return (pntr);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: Takes a pointer to a char as a parameter.
+ *
+ * Return: returns an integer.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+		i++;
+	i--;
+	return (i);
+}
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ *
+ * @str: A pointer to a string.
+ *
+ * Return: A pointer to a string.
+ */
+char *_strdup(char *str)
+{
+	char *pstr;
+	int len;
+
+	if (str == NULL)
+		return (NULL);
+	for (len = 0; str[len] != '\0';)
+		len++;
+	len++;
+	pstr = malloc(len * sizeof(*pstr));
+	if (pstr == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		pstr[len] = str[len];
+		len++;
+	}
+	pstr[len] = '\0';
+	str = pstr;
+	return (str);
 }
