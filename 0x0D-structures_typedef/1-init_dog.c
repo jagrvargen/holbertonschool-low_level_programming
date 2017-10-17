0e41050e5e109224cd9e46adfb0202e63c9aba7d
jesse.hedden <229@holbertonschool.com>
2017-10-17 04:14:34 +0000

    added file init_dog.c with readme update

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a variable of the type struct dog.
+ *
+ * @d: A struct of type dog.
+ * @name: A pointer to an array of chars.
+ * @age: A floating point integer.
+ * @owner: A pointer to an array of integers.
+ *
+ * Return: void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0D-structures_typedef/README.md
+++ 0x0D-structures_typedef/README.md
@@ -1,1 +1,3 @@
 0. dog - A typdef of a struct called 'dog' with the elements char * name, float age, and char *owner.
+
+1. init_dog.c - Initializes a variable of type struct dog.
