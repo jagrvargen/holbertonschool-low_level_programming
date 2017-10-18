3b74a7c0d5889ca1080f135b0f6303c00ec2f60a
jesse.hedden <229@holbertonschool.com>
2017-10-18 11:36:21 +0000

    added file print_name.c

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ *
+ * @name: A pointer to a string.
+ * @f: A pointer to a function.
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
