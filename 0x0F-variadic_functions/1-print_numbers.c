e39523b4f7a178f7a2aa9b667e54ac3a5d47be19
jesse.hedden <229@holbertonschool.com>
2017-10-20 19:35:09 +0000

    added file print_numbers.c with readme and header updates

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ *
+ * @separator: A pointer to a constant string.
+ * @n: An unsigned integer value indicating argument number.
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (separator == NULL || n == 0)
+		return;
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
+}
