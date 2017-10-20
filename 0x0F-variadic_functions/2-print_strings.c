da5ddf36dd56ad773d82a2a9a88a5c3e2a14be77
jesse.hedden <229@holbertonschool.com>
2017-10-20 22:10:10 +0000

    added file print_strings.c with readme and header updates

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - Prints strings, followed by a new line.
+ *
+ * @separator: A pointer to a string to be printed between args.
+ * @n: The number of strings to be passed to the function
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *point;
+
+	if (n == 0)
+		return;
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		point = va_arg(valist, char*);
+		if (point != NULL)
+			printf("%s", point);
+		else
+			printf("(nil)");
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
+}
--- 0x0F-variadic_functions/README.md
+++ 0x0F-variadic_functions/README.md
@@ -1,1 +1,3 @@
 0. sum_them_all.c - A function that returns the sum of all its parameters.
+
+1. print_strings - Prints strings, followed by a new line.
