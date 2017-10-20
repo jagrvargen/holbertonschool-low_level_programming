7f72c5dfa74a845cdffa3f18b5afe1e33395100f
jesse.hedden <229@holbertonschool.com>
2017-10-20 19:17:47 +0000

    added file sum_them_all.c with new readme and header file variadic_functions.h

---
+++ 0x0F-variadic_functions/README.md
@@ -0,0 +1,1 @@
+0. sum_them_all.c - A function that returns the sum of all its parameters.
---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCS
+#define VARIADIC_FUNCS
+#include <stdarg.h>
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
