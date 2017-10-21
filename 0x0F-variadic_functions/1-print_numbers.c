1280c1cc52c5ee08c508b456084efcc48e2600a2
jesse.hedden <229@holbertonschool.com>
2017-10-21 20:26:24 +0000

    removed unnecessary check for 0

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,8 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	if (n == 0)
-		return;
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
