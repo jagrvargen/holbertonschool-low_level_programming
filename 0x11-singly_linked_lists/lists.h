cf65c1a11bfd2118f73bb6be5211e749cf0dca08
jesse.hedden <229@holbertonschool.com>
2017-10-26 20:45:31 +0000

    updated struct typedef with comments

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -4,15 +4,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-typedef struct list list_t;
-
-typedef struct list {
-        char *str;
-        int len;
-        list_t *next;
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
 size_t print_list(const list_t *h);
+
+size_t list_len(const list_t *h);
 
 int _putchar(char c);
 
