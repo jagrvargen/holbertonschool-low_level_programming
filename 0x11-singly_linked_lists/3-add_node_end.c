58b0575b25886f0089bdf5362aa8eb9eb74ebe70
jesse.hedden <229@holbertonschool.com>
2017-10-27 03:16:49 +0000

    added file add_node_end.c with readme and header update

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+int _strlen(char *s);
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ *
+ * @head: A pointer to a pointer.
+ * @str: A pointer to a string.
+ *
+ * Return: A pointer to a node.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *pointer;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+	pointer = *head;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		while (pointer->next != NULL)
+			pointer = pointer->next;
+	}
+	pointer->next = new_node;
+	return (new_node);
+}
+
+/**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: A pointer to a string.
+ *
+ * Return: An integer value.
+ */
+int _strlen(char *s)
+{
+        unsigned int i;
+
+        for (i = 0; s[i] != '\0';)
+                i++;
+
+        return (i);
+}
--- 0x11-singly_linked_lists/README.md
+++ 0x11-singly_linked_lists/README.md
@@ -4,3 +4,5 @@
 1. list_len.c - A function that returns the number of elements in a linked list_t list.
 
 2. add_node.c - A function that adds a new node at the beginning of a list_t list.
+
+3. add_node_end.c - Adds a new node at the end of a list_t list.
