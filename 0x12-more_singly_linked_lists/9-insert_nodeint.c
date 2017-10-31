c0d3b8f5e1049067477ccae5809b36b200a0e9a2
jesse.hedden <229@holbertonschool.com>
2017-10-31 02:33:32 +0000

    added memory free in case of NULL return

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,5 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		pointer->next = new_node;
 		return (new_node);
 	}
+	if (tracker != idx)
+		free(new_node);
 	return (NULL);
 }
