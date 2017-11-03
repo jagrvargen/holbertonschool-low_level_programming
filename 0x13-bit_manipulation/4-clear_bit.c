aeca91bcffd5210d11b2e2afeafb49af04e95aa1
jesse.hedden <229@holbertonschool.com>
2017-11-03 15:52:21 +0000

    added check for null index

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -12,14 +12,13 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
-	if (index > 32)
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 	if (n == NULL)
 		return (-1);
 	mask = 1;
 	while (index--)
 		mask <<= 1;
-	if (*n &= ~mask)
-		return (1);
-	return (-1);
+	*n &= ~mask;
+	return (1);
 }
