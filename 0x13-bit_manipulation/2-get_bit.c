4e6e10be69f6b3951e16216e609987c113c7c93d
jesse.hedden <229@holbertonschool.com>
2017-11-03 02:57:32 +0000

    added file get_bit.c with readme and header update

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index.
+ *
+ * @n: An unsigned long integer value.
+ * @index: An unsigned integer value.
+ *
+ * Return: An integer value.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = 1;
+	while (index--)
+		mask <<= 1;
+	if (mask & n)
+		return (1);
+	else if (!(mask & n))
+		return (0);
+	else
+		return (-1);
+}
--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -8,4 +8,6 @@ int _putchar(char c);
 
 void print_binary(unsigned long int n);
 
+int get_bit(unsigned long int n, unsigned int index);
+
 #endif
