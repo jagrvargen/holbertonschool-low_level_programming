d13042e54697eca550d605d3809a24a6317ffd65
jesse.hedden <229@holbertonschool.com>
2017-11-07 23:07:16 +0000

    fixed open flag order

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	for (len = 0; text_content[len] != '\0';)
 		len++;
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (fd);
 	result = write(fd, text_content, len);
