a18dc709a5bfded26d96864933e713f98d567d70
jesse.hedden <229@holbertonschool.com>
2017-11-07 23:02:28 +0000

    added check for empty string

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,6 +15,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		if (fd == -1)
+			return (fd);
+		result = write(fd, "", 0);
+		if (result == -1)
+			return (result);
+	}
 	for (len = 0; text_content[len] != '\0';)
 		len++;
 	buf = malloc(sizeof(char) * len);
