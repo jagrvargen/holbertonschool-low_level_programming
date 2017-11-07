b96775f73ab18a03c0dec7f344cccd214a5fd715
jesse.hedden <229@holbertonschool.com>
2017-11-07 23:52:00 +0000

    added file append_text_to_file.c with readme and header updates

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - Appends a text at the end of a file.
+ *
+ * @filename: A pointer to text file.
+ * @text_content: A pointer to a string to append to end of file.
+ *
+ * Return: An integer 1 if success, -1 if failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len, err;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd < 0)
+		return (fd);
+	for (len = 0; text_content[len] != '\0';)
+		len++;
+	err = write(fd, text_content, len);
+	if (err < 0)
+		return (err);
+	return (1);
+}
--- 0x14-file_io/README.md
+++ 0x14-file_io/README.md
@@ -3,3 +3,5 @@
 0. read_textfile.c - A function that reads a text file and prints it to the POSIX standard output.
 
 1. create_file.c - Creates a file.
+
+2. append_text_to_file.c - Appends text at the end of a file.
