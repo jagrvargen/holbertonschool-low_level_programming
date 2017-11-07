99e9ba1443f602c7f24284434c336e47be0a5a3e
jesse.hedden <229@holbertonschool.com>
2017-11-07 23:11:24 +0000

    only write if text_content not empty

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,7 +19,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (fd);
-	result = write(fd, text_content, len);
+	if (len > 0)
+		result = write(fd, text_content, len);
 	if (result == -1)
 		return (result);
 	close(fd);
