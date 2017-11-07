1255c3a85eaebd1dabbe3be11d4a45193d5754df
jesse.hedden <229@holbertonschool.com>
2017-11-07 22:52:49 +0000

    added truncate flag and removed null check for empty text_content file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,14 +13,14 @@ int create_file(const char *filename, char *text_content)
 	int result, fd, len;
 	char *buf;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	for (len = 0; text_content[len] != '\0';)
 		len++;
 	buf = malloc(sizeof(char) * len);
 	if (buf == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (fd);
 	result = write(fd, text_content, len);
