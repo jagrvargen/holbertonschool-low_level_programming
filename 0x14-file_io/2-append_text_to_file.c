98082f9a04fefaacf3bd81ce03d77e61fd09ff8e
jesse.hedden <229@holbertonschool.com>
2017-11-08 19:09:24 +0000

    added check for null text file

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -16,13 +16,17 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (fd);
+	if (text_content == NULL)
+		return (-1);
 	for (len = 0; text_content[len] != '\0';)
 		len++;
 	err = write(fd, text_content, len);
 	if (err < 0)
 		return (err);
-	close(fd);
+	err = close(fd);
+	if (err == -1)
+		return (-1);
 	return (1);
 }
