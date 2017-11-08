9237c26490a72edd125c8ef33453239d4e3cbd8a
jesse.hedden <229@holbertonschool.com>
2017-11-08 19:02:51 +0000

    added check for empty text_content file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,17 +14,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	for (len = 0; text_content[len] != '\0';)
-		len++;
+	if (text_content == NULL)
+		len = 0;
+	else
+		for (len = 0; text_content[len] != '\0';)
+			len++;
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (fd);
-	if (len > 0)
-		result = write(fd, text_content, len);
+	result = write(fd, text_content, len);
 	if (result == -1)
 		return (result);
 	result = close(fd);
 	if (result == -1)
-		return (-1)
+		return (-1);
 	return (1);
 }
