7ff09f1a13bb7ea9e1dd685b8daa1a29d99cb45f
jesse.hedden <229@holbertonschool.com>
2017-11-08 19:15:34 +0000

    changed null return to 1

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (fd);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	for (len = 0; text_content[len] != '\0';)
 		len++;
 	err = write(fd, text_content, len);
