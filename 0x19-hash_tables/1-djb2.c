8d45528e26e20806e0125227fdde60d616e7e4c0
jagrvargen <jagarthewolf@gmail.com>
2018-01-11 23:56:04 +0000

    added file 1-djb2.c with readme and header update

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,12 @@
+#include "hash_table"
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+        unsigned long hash = 5381;
+        int c;
+
+        while (c = *str++)
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+        return hash;
+}
--- 0x19-hash_tables/README.md
+++ 0x19-hash_tables/README.md
@@ -1,2 +1,4 @@
 			      Hash Tables
 0. 0-hash_table_create.c - A function which creates a hash table.
+
+1. 1-djb2.c - A hash function implementing the djb2 algorithm.
