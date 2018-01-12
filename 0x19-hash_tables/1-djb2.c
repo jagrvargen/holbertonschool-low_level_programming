fd19d7b3b0ff43feaa41c0a31a68604532ea8cb5
jagrvargen <jagarthewolf@gmail.com>
2018-01-12 21:33:57 +0000

    added comments

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - A hashing function.
+ *
+ * @str: A string value which is the key belonging to a node.
+ *
+ * Return: A hash developed from the key.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
         unsigned long int hash = 5381;
