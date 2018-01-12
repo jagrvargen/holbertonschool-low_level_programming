ad678b67d079c513c1a8999712ee8ed659f76129
jagrvargen <jagarthewolf@gmail.com>
2018-01-12 21:55:44 +0000

    added check for empty hash table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -17,6 +17,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
+	if (ht->size == NULL || ht->array == NULL)
+		return (0);
+
 	if (key == '\0') /* Check for empty key */
 		return (0);
 
