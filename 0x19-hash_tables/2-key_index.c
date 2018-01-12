83f11a4f55549e15382c97caaef79656ab7738b8
jagrvargen <jagarthewolf@gmail.com>
2018-01-12 21:31:09 +0000

    added comments

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - A function that gives the index of a key.
+ *
+ * @key: A pointer to a string containing the key.
+ * @size: An unsigned long integer indicating the size of the array.
+ *
+ * Return: An unsigned long integer representing the array's index.
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
