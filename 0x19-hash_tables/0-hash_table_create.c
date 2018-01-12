92dcc21215755c9f76173a323f551ec61e2af04e
jagrvargen <jagarthewolf@gmail.com>
2018-01-12 19:03:09 +0000

    fixed memory allocation error for array members

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -3,17 +3,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *my_table;
+	unsigned long int i;
 
 	my_table = malloc(sizeof(hash_table_t));
 	if (my_table == NULL)
 		return (NULL);
 
-	my_table->array = malloc(sizeof(hash_node_t*) * size);
+	my_table->array = malloc(sizeof(void*) * size);
 	if (my_table->array == NULL)
 	{
 		free(my_table);
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		my_table->array[i] = NULL;
 
 	my_table->size = size;
 
