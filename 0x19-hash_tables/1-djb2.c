d6599ec26b10f3fb5c23c598a6c49f6c21dbed31
jagrvargen <jagarthewolf@gmail.com>
2018-01-12 00:22:10 +0000

    added missing parentheses

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,12 +1,12 @@
-#include "hash_table"
+#include "hash_tables.h"
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-        unsigned long hash = 5381;
+        unsigned long int hash = 5381;
         int c;
 
-        while (c = *str++)
+        while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-        return hash;
+        return (hash);
 }
