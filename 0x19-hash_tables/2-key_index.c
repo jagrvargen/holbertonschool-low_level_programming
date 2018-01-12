c2ea31f204897f077058277b8da6d12670250a43
jagrvargen <jagarthewolf@gmail.com>
2018-01-12 00:23:17 +0000

    added 2-key_index.c with readme update

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+	index %= size;
+
+	return (index);
+}
--- 0x19-hash_tables/README.md
+++ 0x19-hash_tables/README.md
@@ -2,3 +2,5 @@
 0. 0-hash_table_create.c - A function which creates a hash table.
 
 1. 1-djb2.c - A hash function implementing the djb2 algorithm.
+
+2. 2-key_index.c - A function that gives you the index of a key.
