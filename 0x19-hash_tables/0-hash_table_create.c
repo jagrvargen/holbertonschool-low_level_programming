7615e76bb0f83de5ebd91726204bde3854ee9da8
jagrvargen <jagarthewolf@gmail.com>
2018-01-13 02:59:12 +0000

    fixed betty style error

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (my_table == NULL)
 		return (NULL);
 
-	my_table->array = malloc(sizeof(void*) * size);
+	my_table->array = malloc(sizeof(void *) * size);
 	if (my_table->array == NULL)
 	{
 		free(my_table);
