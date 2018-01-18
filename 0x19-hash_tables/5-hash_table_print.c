d1fabed6efd850349b583dd76d610aea9fcedd2b
jagrvargen <jagarthewolf@gmail.com>
2018-01-18 20:52:22 +0000

    fixed comma print error

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -18,19 +18,22 @@ void hash_table_print(const hash_table_t *ht)
 
 	printf("{");
 
-	flag = 0;
+	flag = 1;
 	for (index = 0; index < ht->size; index++)
 	{
 		temp = ht->array[index];
-		if (flag == 1)
-			printf(", ");
 		while (temp)
 		{
-			printf("'%s': '%s'", temp->key, temp->value);
+			if (flag == 1)
+				printf("'%s': '%s'", temp->key, temp->value);
+			else
+			{
+				printf(", ");
+				printf("'%s': '%s'", temp->key, temp->value);
+			}
 			temp = temp->next;
+			flag = 0;
 		}
-		flag = 0;
 	}
-
 	printf("}\n");
 }
