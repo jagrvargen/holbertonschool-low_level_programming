c1817ace67593b81178f269caab088e0a60b253c
jagrvargen <jagarthewolf@gmail.com>
2018-01-28 00:27:34 +0000

    added 3-quick_sort.c

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,81 @@
+#include "sort.h"
+
+/**
+ * partition - A partitioning algorithm.
+ *
+ * @array: An array of integers.
+ * @low: An unsigned integer value.
+ * @high: An unsigned integer value.
+ *
+ * Return: an integer that is the partition of the calling quicksort
+ */
+size_t partition(int *array, int low, int high)
+{
+	int pivot;
+	int i, j;
+	int temp;
+
+	pivot = array[high];
+	i = low;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i += 1;
+		}
+	}
+/*	if (array[high] < array[i + 1])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, 10);
+		}*/
+	temp = array[i];
+	array[i] = array[high];
+	array[high] = temp;
+	print_array(array, 10);
+	return (i);
+}
+
+/**
+ * q_sort - A helper function for quicksort to allow for passing low & high
+ *
+ * @array: An array of integers
+ * @low: the lowest index number being sorted
+ * @high: the highest index number being sorted
+ *
+ * Return: void
+ */
+void q_sort(int *array, int low, int high)
+{
+	int p;
+
+	if (low < high)
+	{
+		p = partition(array, low, high);
+		q_sort(array, low, p - 1);
+		q_sort(array, p + 1, high);
+	}
+}
+
+
+/**
+ * quick_sort - An implementation of the Quicksort algorithm.
+ *
+ * @array: An array of integers.
+ * @size: An unsigned integer value indicating the size of the array.
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	int low = 0;
+	int high = size - 1;
+
+	q_sort(array, low, high);
+}
