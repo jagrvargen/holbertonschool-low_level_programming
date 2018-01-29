1e471e272e8faa7fb5212f7eb475d7bf9e30424f
jagrvargen <jagarthewolf@gmail.com>
2018-01-29 00:31:10 +0000

    added NULL check

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -1,11 +1,21 @@
 #include "sort.h"
 
+/**
+ * selection_sort - An implementation of the selection sort algorithm.
+ *
+ * @array: An array of integers
+ * @size: A unsigned integer value
+ *
+ * Return: void
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 	size_t min;
 	int hold;
 
+	if (array == NULL)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
@@ -17,6 +27,7 @@ void selection_sort(int *array, size_t size)
 		hold = array[min];
 		array[min] = array[i];
 		array[i] = hold;
-		print_array(array, size);
+		if (array[min] != array[i])
+			print_array(array, size);
 	}
 }
