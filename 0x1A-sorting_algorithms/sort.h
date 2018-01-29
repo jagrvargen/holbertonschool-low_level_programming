4706e7a1719072c2a14b1ec7f7ce6d0ed9708d45
jagrvargen <jagarthewolf@gmail.com>
2018-01-29 00:40:16 +0000

    fixed prototype definitions

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -23,4 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 
+void quick_sort(int *array, size_t size);
+void q_sort(int *array, int high, int low);
+size_t partition(int *array, int high, int low);
 #endif
