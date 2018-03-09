df7e4bff2d53b66db56118b7b902dc01b15cf008
jagrvargen <jagarthewolf@gmail.com>
2018-03-09 20:31:07 +0000

    removed unnecessary helper function

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -21,22 +21,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left && tree->right)
-		check = is_full(tree);
-	return (check);
-}
-
-/**
- * is_full - Checks if both branches of tree are full.
- *
- * @tree: A pointer to the root node.
- *
- * Return: An integer 1 for full, 0 otherwise.
- */
-int is_full(const binary_tree_t *tree)
-{
-	int check;
-
-	if (tree->left && tree->right)
-		check = is_full(tree->left) && is_full(tree->right);
-	return (check);
+	{
+		check = binary_tree_is_full(tree->left) &&\
+			binary_tree_is_full(tree->right);
+		return (check);
+	}
+	return (0);
 }
