a85cdb7c0290885d950ac6b464b68e96c347c809
jagrvargen <jagarthewolf@gmail.com>
2018-03-09 00:13:32 +0000

    simplified algo

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -9,31 +9,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (!tree->left)
-	{
-		if (!tree->right)
-			leaves++;
-	}
+	if (!tree->left && !tree->right)
+		return (1);
 
-	if (tree->left)
-	{
-		binary_tree_leaves(tree->left);
-		if (tree->right)
-			binary_tree_leaves(tree->right);
-		leaves++;
-	}
-	if (tree->right)
-	{
-		binary_tree_leaves(tree->right);
-		if (tree->left)
-			binary_tree_leaves(tree->left);
-		leaves++;
-	}
-
-	return (leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
