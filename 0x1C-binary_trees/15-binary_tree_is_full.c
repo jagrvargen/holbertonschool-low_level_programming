f88440b95b83f890638b31c72d3298fa910ea18c
jagrvargen <jagarthewolf@gmail.com>
2018-03-09 20:26:01 +0000

    fixed algo

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-
+int is_full(const binary_tree_t *tree);
 /**
  * binary_tree_is_full - Checks if a binary tree is full.
  *
@@ -21,10 +21,22 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left && tree->right)
-	{
-		check = binary_tree_is_full(tree->left);
-		check = binary_tree_is_full(tree->right);
-		return (check);
-	}
-	return (0);
+		check = is_full(tree);
+	return (check);
+}
+
+/**
+ * is_full - Checks if both branches of tree are full.
+ *
+ * @tree: A pointer to the root node.
+ *
+ * Return: An integer 1 for full, 0 otherwise.
+ */
+int is_full(const binary_tree_t *tree)
+{
+	int check;
+
+	if (tree->left && tree->right)
+		check = is_full(tree->left) && is_full(tree->right);
+	return (check);
 }
